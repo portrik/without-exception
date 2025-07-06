f933058ebbe8a9bcaa7ce8f8da2269f5bc9828c5
Patrik Dvořáček <patrik@dvoracek.dev>
2025-07-06 15:27:22 +0200

    docs(docs): Add basic docs with deployment.

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+	title: "Without Exception",
+	description:
+		"Zero-dependency functional utilities for TypeScript inspired by Gleam.",
+	srcDir: "src",
+	outDir: "dist",
+});
